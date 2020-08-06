76bfae34f76845efbcdd741cc20a9cbf5306ac41
KeshavPrabhakar <30862843+KeshavPrabhakar@users.noreply.github.com>
2020-08-07 01:05:48 +0530

    Add files via upload

---
+++ StudentAPI-2/src/test/java/com/cravejava/app/StudentApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cravejava.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
